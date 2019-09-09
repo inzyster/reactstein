680d35dfd47b35620707ad97bdc3a68c475bf94b
Tomasz Chodakowski <inzyster@gmail.com>
2019-09-09 23:56:19 +0200

    More stuff

---
+++ src/Position.ts
@@ -0,0 +1,11 @@
+export default class Position {
+  public x: number;
+  public y: number;
+  public angle: number;
+
+  constructor() {
+    this.x = 0.0;
+    this.y = 0.0;
+    this.angle = 0.0;
+  }
+}
--- src/Utils.ts
+++ src/Utils.ts
@@ -1,1 +1,23 @@
+import { useRef, useEffect } from 'react';
+
 export const getUrl = (url: string) => (process.env.NODE_ENV === 'development' ? url : `/reactstein/${url}`);
+
+/// source: https://css-tricks.com/using-requestanimationframe-with-react-hooks/
+export const useAnimationFrame = (callback: (delta: number) => void) => {
+  const requestRef = useRef<number>(0);
+  const previousTimeRef = useRef<number | undefined>();
+
+  const animate = (time: number) => {
+    if (previousTimeRef.current !== undefined) {
+      const delta = time - previousTimeRef.current;
+      callback(delta);
+    }
+    previousTimeRef.current = time;
+    requestRef.current = requestAnimationFrame(animate);
+  };
+
+  useEffect(() => {
+    requestRef.current = requestAnimationFrame(animate);
+    return () => cancelAnimationFrame(requestRef.current);
+  }, []);
+};
